cced919fa4e9cf08192a6d04d5933f4e576b45fd
vmenpn <89683922+vmenpn@users.noreply.github.com>
2021-11-13 14:44:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const getLink = require('./getlink');
-const port = 8080;
+const port = 8000;
 
 getLink.getCookieCsrf().then(() => {
   app.set('view engine', 'ejs');
